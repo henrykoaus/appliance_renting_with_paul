e5b61100403e45d8508042e476f531851f9af860
PAUL PENG <paul618300@gmail.com>
2025-03-18 15:34:31 +1100

    add testing data by running seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+import "faker"
+require 'net/http'
+require 'json'
+
+Appliance.destroy_all
+#
+20.times do
+  url = URI("https://api.unsplash.com/photos/random?client_id=4SNI-X9XtwrDDQH3JrvQfknmNtuvpnrsVemUhP1uPGk&query=house&orientation=landscape")
+
+  begin
+    response = Net::HTTP.get(url)
+    data = JSON.parse(response)
+  rescue StandardError => e
+    { error: "Failed to fetch image: #{e.message}" }
+  end
+  Appliance.create!(
+    photo_url: data['urls']['raw'],
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    availability: "March 19-April 1",
+    price:(50..100).to_a.sample,
+    overview:  Faker::Lorem.sentence,
+    user: User.all.sample,
+  )
+end
+
+
